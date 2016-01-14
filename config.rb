62f103d2cf04d71a5a4fb1d8db9c8dc058e08fa1
pig <qkqkzzzz@gmail.con>
2016-01-14 10:51:43 +0800

    add files

---
+++ config.rb
@@ -0,0 +1,30 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "src/css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+extensions_dir = "sass-extensions"
+
+output_style = :expanded
+environment = :development
+
+relative_assets = true
+sass_options = {}
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :scss
+
+css_dir = "src/css" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "src/img" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
